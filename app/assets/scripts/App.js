30fd1bec965def5910e4564e9cdd0d0cf25aee03
elricad1308 <jc.aguilar1308@gmail.com>
2017-02-07 23:05:47 -0600

    Initialized page scripting.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,9 @@
+function Person(name, favColor) {
+    console.log("Hello, my name is " + name + " and my favorite color is " + favColor + ".");
+}
+
+var johnName = "John Doe";
+var johnColor = "blue";
+
+Person(johnName, johnColor);
+Person('Jane Smith', 'green');
